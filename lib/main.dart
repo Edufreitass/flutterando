f95e2af5a19afad69fe15fb7f051ebd632e5578d
Eduardo Freitas <eduardoflorencio96@gmail.com>
2023-06-28 21:35:28 -0300

    feat: materialapp

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 // Widget = Componentes
 void main() {
-  runApp(AppWidget(title: 'Flutterando',));
+  runApp(AppWidget(
+    title: 'Flutterando 3',
+  ));
 }
 
 class AppWidget extends StatelessWidget {
@@ -12,13 +14,10 @@ class AppWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          title,
-          textDirection: TextDirection.ltr,
-          style: TextStyle(color: Colors.white, fontSize: 50.0),
-        ),
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: Container(
+        child: Center(child: Text('Flutterando')),
       ),
     );
   }
