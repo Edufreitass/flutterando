9ab0280ef5fccbe0c6d863e832e88e3cc0c60c98
Eduardo Freitas <eduardoflorencio96@gmail.com>
2023-06-30 15:15:11 -0300

    feat: stack

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -11,62 +11,77 @@ class _LoginPageState extends State<LoginPage> {
   String email = '';
   String password = '';
 
+  Widget _body() {
+    return SingleChildScrollView(
+      child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    width: 200,
+                    height: 200,
+                    child: Image.asset('assets/images/logo.png')),
+                Container(
+                  height: 20,
+                ),
+                TextField(
+                  onChanged: (text) {
+                    email = text;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  onChanged: (text) {
+                    password = text;
+                  },
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 15),
+                ElevatedButton(
+                  onPressed: () {
+                    if (email == 'eduardo@email.com.br' && password == '123') {
+                      Navigator.of(context).pushReplacementNamed('/home');
+                    } else {
+                      print('login invalido');
+                    }
+                  },
+                  child: Text('Entrar'),
+                )
+              ],
+            ),
+          )),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-            width: MediaQuery.of(context).size.width,
+      body: Stack(
+        children: [
+          SizedBox(
             height: MediaQuery.of(context).size.height,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                      width: 200,
-                      height: 200,
-                      child: Image.asset('assets/images/logo.png')),
-                  Container(
-                    height: 20,
-                  ),
-                  TextField(
-                    onChanged: (text) {
-                      email = text;
-                    },
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  TextField(
-                    onChanged: (text) {
-                      password = text;
-                    },
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  SizedBox(height: 15),
-                  ElevatedButton(
-                    onPressed: () {
-                      if (email == 'eduardo@email.com.br' &&
-                          password == '123') {
-                        Navigator.of(context).pushReplacementNamed('/home');
-                      } else {
-                        print('login invalido');
-                      }
-                    },
-                    child: Text('Entrar'),
-                  )
-                ],
-              ),
-            )),
-      ),
+            child: Image.asset(
+              'assets/images/background.png', 
+              fit: BoxFit.cover)),
+          Container(
+            color: Colors.black.withOpacity(0.3),
+          ),
+          _body(),
+        ],
+      )
     );
   }
 }
