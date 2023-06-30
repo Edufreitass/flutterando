25e07374fef019253bc91d33de146689c3e69c40
Eduardo Freitas <eduardoflorencio96@gmail.com>
2023-06-30 15:35:43 -0300

    feat: material card

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -12,76 +12,97 @@ class _LoginPageState extends State<LoginPage> {
   String password = '';
 
   Widget _body() {
+    final ButtonStyle style = ElevatedButton.styleFrom(
+      foregroundColor: Colors.white,
+      backgroundColor: Colors.red,
+    );
     return SingleChildScrollView(
       child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                    width: 200,
-                    height: 200,
-                    child: Image.asset('assets/images/logo.png')),
-                Container(
-                  height: 20,
-                ),
-                TextField(
-                  onChanged: (text) {
-                    email = text;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  width: 200,
+                  height: 200,
+                  child: Image.asset('assets/images/logo.png')),
+              Container(
+                height: 20,
+              ),
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      left: 12, right: 12, top: 20, bottom: 12),
+                  child: Column(
+                    children: [
+                      TextField(
+                        onChanged: (text) {
+                          email = text;
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      TextField(
+                        onChanged: (text) {
+                          password = text;
+                        },
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      ElevatedButton(
+                        style: style,
+                        onPressed: () {
+                          if (email == 'eduardo@email.com.br' &&
+                              password == '123') {
+                            Navigator.of(context).pushReplacementNamed('/home');
+                          } else {
+                            print('login invalido');
+                          }
+                        },
+                        child: Container(
+                          width: double.infinity,
+                          child: Text(
+                            'Entrar',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                SizedBox(height: 10),
-                TextField(
-                  onChanged: (text) {
-                    password = text;
-                  },
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 15),
-                ElevatedButton(
-                  onPressed: () {
-                    if (email == 'eduardo@email.com.br' && password == '123') {
-                      Navigator.of(context).pushReplacementNamed('/home');
-                    } else {
-                      print('login invalido');
-                    }
-                  },
-                  child: Text('Entrar'),
-                )
-              ],
-            ),
-          )),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          SizedBox(
+        body: Stack(
+      children: [
+        SizedBox(
             height: MediaQuery.of(context).size.height,
-            child: Image.asset(
-              'assets/images/background.png', 
-              fit: BoxFit.cover)),
-          Container(
-            color: Colors.black.withOpacity(0.3),
-          ),
-          _body(),
-        ],
-      )
-    );
+            child:
+                Image.asset('assets/images/background.png', fit: BoxFit.cover)),
+        Container(
+          color: Colors.black.withOpacity(0.3),
+        ),
+        _body(),
+      ],
+    ));
   }
 }
